aa3c9b63ce7394a9721a11e6d2c007238a54fd82
Rafael <rafaelcamelo31@gmail.com>
2020-12-24 02:43:33 -0400

    Retorno de json complete e compact no controller

--- api-catalog/controllers/productController.js
+++ api-catalog/controllers/productController.js
@@ -3,12 +3,22 @@ import Product from "../models/productModels.js"
 // @route GET /api/product/:id
 const getProductById = (async (req, res) => {
     try {
-        const product = await Product.findOne({
+        const complete = await Product.findOne({
             id: req.params.id
         });
 
-        if (product) {
-            res.json(product);
+        const compact = await Product.findOne({
+            id: req.params.id
+        }, { name: 1, price: 1, status: 1, categories: 1 });
+
+        if (complete && compact) {
+            res.json(
+                {
+                    complete,
+                    compact
+                }
+            );
+
         } else {
             res.status(404);
             throw new Error("Product not found!");
--- api-catalog/models/productModels.js
+++ api-catalog/models/productModels.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 
-const productSchema = mongoose.Schema({
+const productSchema = mongoose.Schema({});
 
-});
-
-const Product = mongoose.model("Product", productSchema);
+const Product = mongoose.model("Product", productSchema,);
 
 export default Product;
